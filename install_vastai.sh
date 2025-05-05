4159c4e302c12a4a73540043884088502cb3be16
yushan777 <123392471+yushan777@users.noreply.github.com>
2025-05-05 10:53:38 +0100

    Create install_vastai.sh
    ..

---
+++ install_vastai.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Prompt for HF token
+read -sp "Enter your Hugging Face access token: " HF_TOKEN
+echo
+
+# clone repo
+git clone https://github.com/yushan777/SUPIR.git
+# cd 
+
+# install deps
+pip install torch==2.7.0+cu126 torchvision==0.22.0+cu126 torchaudio==2.7.0+cu126 --extra-index-url https://download.pytorch.org/whl/cu126
+pip install -r requirements.txt
+
+# Install Hugging Face CLI and related packages
+pip install -U "huggingface_hub[cli]"
+pip install huggingface_hub[hf_transfer]
+pip install hf_transfer
+
+# Log in to Hugging Face CLI with your token
+huggingface-cli login --token $HF_TOKEN
+export HF_HUB_ENABLE_HF_TRANSFER=1
+REPO_NAME="yushan777/SUPIR"
+
+# download models
+huggingface-cli download $REPO_NAME SUPIR/SUPIR-v0Q_fp32.safetensors --local-dir downloads
+huggingface-cli download $REPO_NAME SUPIR/SUPIR-v0F_fp32.safetensors --local-dir downloads
+huggingface-cli download $REPO_NAME SDXL/juggernautXL_v9Rundiffusionphoto2.safetensors --local-dir downloads
+huggingface-cli download $REPO_NAME CLIP1/clip-vit-large-patch14/safetensors/clip-vit-large-patch14.safetensors --local-dir downloads
+huggingface-cli download $REPO_NAME CLIP2/CLIP-ViT-bigG-14-laion2B-39B-b160k/safetensors/CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors --local-dir downloads
+
+# move the models
+mv downloads/SUPIR/SUPIR-v0Q_fp32.safetensors SUPIR/models/SUPIR/
+mv downloads/SUPIR/SUPIR-v0F_fp32.safetensors SUPIR/models/SUPIR/
+mv downloads/SDXL/juggernautXL_v9Rundiffusionphoto2.safetensors SUPIR/models/SDXL
+mv downloads/CLIP1/clip-vit-large-patch14/safetensors/clip-vit-large-patch14.safetensors SUPIR/models/CLIP1
+mv downloads/CLIP2/CLIP-ViT-bigG-14-laion2B-39B-b160k/safetensors/CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors SUPIR/models/CLIP2
+
