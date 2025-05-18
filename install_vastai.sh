b4f5b5ec716b1eeccf73591472f7fda5a9f2bbe2
yushan777 <yushanyu2001@gmail.com>
2025-05-18 14:02:49 +0100

    Update install_vastai.sh
    ...

--- install_vastai.sh
+++ install_vastai.sh
@@ -5,6 +5,7 @@
 sudo apt update
 sudo apt install -y libgl1
 
+# no venv required
 pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --extra-index-url https://download.pytorch.org/whl/cu126
 # pip install torch==2.7.0+cu126 torchvision==0.22.0+cu126 torchaudio==2.7.0+cu126 --extra-index-url https://download.pytorch.org/whl/cu126
 pip install -r requirements.txt
